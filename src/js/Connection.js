c066f617712e344aba5ac769d6e2c35da66fe234
olyaaas42 <olyaaas42@gmail.com>
2023-06-18 18:35:05 +0700

    Validate

---
+++ src/js/Connection.js
@@ -0,0 +1,9 @@
+<script src="https://unpkg.com/axios/dist/axios.min.js "></script>
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+
+    }
+}
